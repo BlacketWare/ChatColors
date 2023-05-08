bb685e28cf18318ae31c8fae72e919f6a8355c72
VR <101288516+VillainsRule@users.noreply.github.com>
2023-05-08 16:28:09 -0400

    fix

--- index.mjs
+++ index.mjs
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
 app.use(express.static('static'));
@@ -13,7 +18,7 @@ app.use(compression({
     level: 9
 }));
 
-app.get('/', async (req, res) => res.send(fs.readFileSync('./static/index.html', 'utf8')));
+app.get('/', async (req, res) => res.send(path.join(__dirname + '/static/index.html')));
 app.get('/worker/blooks', async (req, res) => res.json(JSON.parse(fs.readFileSync('./blooks.json', 'utf8'))));
 
 app.listen(8080, () => console.log('Listening on port 8080'));
